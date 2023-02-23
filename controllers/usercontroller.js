884de3d0bcc6537e713a74c72236f5b3da9c74a8
Jailene <jaileneveras8@gmail.com>
2023-02-23 16:48:28 -0500

    added updateuser, deleteuser and createuser to usercontroller.js

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -11,7 +11,39 @@ const userController = {
                 console.log(err)
                 res.status(500).json(err)
             })
-    } 
+    } ,
+    createUser(req, res){
+        User.create(req, res) 
+            User.create(req.body)
+                .then((userData)=>{
+                    res.json(userData)
+                })
+                .catch((err)=>{
+                    console.log(err)
+                    res.status(500).json(err);
+                })
+    },updateUser (req, res){
+        User.updateOne(
+            {_id: req.params.id},
+            {$set: req.body},
+            { runValidators: true, new: true }
+        ) .then ((userData)=>{
+            res.json(userData)
+        })
+        .catch((err)=>{
+            res.status(500).json(err);
+        })
+        
+    }, deleteUser (req,res){
+        User.findOneAndDelete(
+            {_id : req.params.id}
+        )   .then((userData)=>{
+            res.json(userData)
+        }) .catch((err)=>{
+            res.status(500).json(err)
+        })
+    }
 }
+
 
 module.exports= userController
