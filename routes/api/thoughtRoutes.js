12783cd318ef039a49853a08efb6ec37cd974ec2
Jailene <jaileneveras8@gmail.com>
2023-02-25 23:14:47 -0500

    defined rest of the routes in thoughtsRoutes.js

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -15,6 +15,13 @@ router.route('/').get(getThoughts).post(createThought);
 
 router.route('/:userId').get(getsingleThought);
 
+router.route('/:userId').put(updateThought);
+
+router.route('/userId').delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions').delete(removeReaction);
 
 
 
