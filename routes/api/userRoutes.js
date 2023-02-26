9536a78b47a34029a0fbdead1c2902289d2e9c21
Jailene <jaileneveras8@gmail.com>
2023-02-25 23:24:49 -0500

    defined routes in userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,8 +3,16 @@ const router = require("express").Router()
 const {
     getUsers,
     createUser,
+    getSingleUser,
+    updateUser,
+    deleteUser
 } = require("../../controllers/usercontroller")
 
 
 
 router.route("/").get(getUsers).post(createUser)
+
+router.route("/:userId").get(getSingleUser)
+router.route("/:userId").put(updateUser)
+
+router.route("/:userId").delete(deleteUser)
